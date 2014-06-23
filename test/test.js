2222498edf1bf204ac799d1a26d84a332c2d70de
Daijiro Wachi <daijiro.wachi@gmail.com>
2014-06-23 19:33:21 +0900

    Update npm test

---
+++ test/test.js
@@ -0,0 +1,17 @@
+var fs = require('fs');
+var path = require('path');
+var jsss = require('../lib/jsss');
+var opt = {
+  encoding: 'utf8'
+};
+var path = {
+  jsss: path.resolve(__dirname + '/style.jsss'),
+  fixture: path.resolve(__dirname + '/fixture.css')
+};
+
+var code = fs.readFileSync(path.jsss, opt);
+var fixture = fs.readFileSync(path.fixture, opt);
+var css = jsss.parse(code);
+
+
+if (css !== fixture) throw Error(';(');
